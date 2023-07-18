e8571dd5ee06ce632c282be98555f1112c370da4
Brendon45 <jejebrendon722@gmail.com>
2023-07-18 16:48:24 -0700

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
-
 /**
- * _abs - contains the absolute value of an integer
- * @c: the number to be counted
- * Return: Absolute value of number or zero
+ * _abs - computes the absolute value of an integrer
+ *
+ * @n: the int to check
+ *
+ * Return: the absolute value of int
+ *
  */
-
-int _abs(int c)
+int _abs(int n)
 {
-if (c < 0)
-{
-int abs_val;
-
-abs_val = c * -1;
-return (abs_val);
-}
-return (c);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
