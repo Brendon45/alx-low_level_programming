9834bd61098d348124a2d93ab734578fe9b883f9
Brendon45 <jejebrendon722@gmail.com>
2023-08-04 16:24:19 -0700

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - Checks string there are digits
+ * @str: Array string
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+
+	unsigned int read;
+
+	read = 0;
+	while (read < strlen(str))
+
+	{
+	if (!isdigit(str[read]))
+	{
+	return (0);
+	}
+
+	read++;
+	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	int read;
+	int str_to_int;
+	int sum = 0;
+
+	read = 1;
+	while (read < argc)
+	{
+	if (check_num(argv[read]))
+
+	{
+	str_to_int = atoi(argv[read])
+	sum += str_to_int;
+	}
+
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+
+	read++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
