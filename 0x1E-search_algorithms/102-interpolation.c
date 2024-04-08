a6f859f7893de575bfed93181d33c7fbadaca97f
Brendon45 <jejebrendon722@gmail.com>
2024-04-08 15:11:05 -0700

    a function that searches for a value in a sorted array of integers using the Interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -3,10 +3,10 @@
 /**
  * interpolation_search - Searches for a value in an array of
  * integers using the Interpolation search algorithm.
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
+ * @array: A pointer to the first element of the array to search
+ * @size: The number of elements in the array
  * @value: The value to search for.
- * Return: If the value is not present or the array is NULL, -1.
+ * Return: If the value is not present or the array is NULL, -1
  *         else, the index where the value is located.
  */
 int interpolation_search(int *array, size_t size, int value)
